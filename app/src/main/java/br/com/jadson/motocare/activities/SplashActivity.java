04485175c847e1a460c8165bb5f1218e27ae2468
Jadson Eduardo <jadsoneduardo216@gmail.com>
2026-08-11 14:30:45 -0300

    feat: conecta splash screen à tela principal

---
+++ app/src/main/java/br/com/jadson/motocare/activities/SplashActivity.java
@@ -0,0 +1,57 @@
+package br.com.jadson.motocare.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import br.com.jadson.motocare.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final long SPLASH_DURATION = 2500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+
+        ViewCompat.setOnApplyWindowInsetsListener(
+                findViewById(R.id.main),
+                (v, insets) -> {
+                    Insets systemBars = insets.getInsets(
+                            WindowInsetsCompat.Type.systemBars()
+                    );
+
+                    v.setPadding(
+                            systemBars.left,
+                            systemBars.top,
+                            systemBars.right,
+                            systemBars.bottom
+                    );
+
+                    return insets;
+                }
+        );
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+
+            Intent intent = new Intent(
+                    SplashActivity.this,
+                    MainActivity.class
+            );
+
+            startActivity(intent);
+            finish();
+
+        }, SPLASH_DURATION);
+    }
+}
